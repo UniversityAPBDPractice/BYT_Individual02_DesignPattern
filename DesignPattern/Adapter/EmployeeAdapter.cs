b6984a897aaa4b63f52fef6de6674ecf42cbd08c
s28345 <milkoaleksandr307@gmail.com>
2025-11-22 14:25:06 +0100

    Implement the patterns

--- DesignPattern/Adapter/EmployeeAdapter.cs
+++ DesignPattern/Adapter/EmployeeAdapter.cs
@@ -1,13 +1,27 @@
-﻿// In case you need some guidance: https://refactoring.guru/design-patterns/adapter
-namespace DesignPattern.Adapter
+﻿namespace DesignPattern.Adapter
 {
     public class EmployeeAdapter : ITarget
     {
         private readonly BillingSystem thirdPartyBillingSystem = new();
 
-        //public void ProcessCompanySalary(...)
-        //{
-        //    ...
-        //}
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            List<Employee> listEmployee = new List<Employee>();
+            
+            for (int i = 0; i < employeesArray.GetLength(0); i++)
+            {
+                int id = int.Parse(employeesArray[i, 0]);
+                string name = employeesArray[i, 1];
+                string designation = employeesArray[i, 2];
+                decimal salary = decimal.Parse(employeesArray[i, 3]);
+
+                listEmployee.Add(new Employee(id, name, designation, salary));
+            }
+
+            Console.WriteLine("Adapter converted Array of Employee to List of Employee");
+            Console.WriteLine("Then delegate to the ThirdPartyBillingSystem for processing the employee salary\n");
+
+            thirdPartyBillingSystem.ProcessSalary(listEmployee);
+        }
     }
 }
